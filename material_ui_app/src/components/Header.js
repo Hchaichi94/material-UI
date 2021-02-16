6a2292ee5e1bec1403424986b568e5ef8b3722c4
akremH <hchaichi-akrem@outlook.fr>
2021-02-16 16:45:35 +0100

    fix appBar

--- material_ui_app/src/components/Header.js
+++ material_ui_app/src/components/Header.js
@@ -5,54 +5,60 @@ import Toolbar from '@material-ui/core/Toolbar';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     root: {
-        flexGrow: 1,
+        backgroundColor: '#fff'
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-}));
+    searchInput: {
+        opacity: '0.6',
+        padding: '0px 8px',
+        fontSize: '0.8rem',
+        '&:hover': {
+            backgroundColor: "#f2f2f2"
+        },
+        '& .MuiSvgIcon-root': {
+            marginRight: '8px'
+        }
+    }
+});
 
 export default function Header() {
 
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Grid container>
-                        <Grid item >
-                            <InputBase />
-                        </Grid>
-                        <Grid item sm></Grid>
-                        <Grid item >
 
-                            <IconButton>
-                                <Badge badgeContent={4} color="secondary">
-                                    <NotificationsNoneIcon />
-                                </Badge>
-                            </IconButton>
-
-                            <IconButton>
-                                <Badge badgeContent={3} color="primary">
-                                    <ChatBubbleOutlineIcon />
-                                </Badge>
-                            </IconButton>
-
-                            <IconButton>
-                                <PowerSettingsNewIcon />
-                            </IconButton>
-                        </Grid>
+        <AppBar position="static" className={classes.root}>
+            <Toolbar>
+                <Grid container alignItems="center">
+                    <Grid item >
+                        <InputBase placeholder="Search topics" className={classes.searchInput} startAdornment={<SearchIcon fontSize="small" />} />
                     </Grid>
-                </Toolbar>
-            </AppBar >
-        </div >
+                    <Grid item sm></Grid>
+                    <Grid item >
+
+                        <IconButton>
+                            <Badge badgeContent={4} color="secondary">
+                                <NotificationsNoneIcon />
+                            </Badge>
+                        </IconButton>
+
+                        <IconButton>
+                            <Badge badgeContent={3} color="primary">
+                                <ChatBubbleOutlineIcon />
+                            </Badge>
+                        </IconButton>
+
+                        <IconButton>
+                            <PowerSettingsNewIcon />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar >
+
     );
 }
